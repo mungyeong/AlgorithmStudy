90ca283d10589cad7ebd25594fbba65b6964a94e
gyeong5961 <gyeong5961@gmail.com>
2021-01-15 20:26:54 +0900

    new Solved Problem 2021/01/15

---
+++ src/baeKjoon/Problem10757.java
@@ -0,0 +1,10 @@
+package baeKjoon;
+
+import java.util.Scanner;
+
+class Problem10757 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextBigInteger().add(sc.nextBigInteger()));
+    }
+}
